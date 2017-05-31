f380a90e30c57907546c1e184cae47e86e956123
Delink-D <delinkdesigns@gmail.com>
2017-06-01 01:08:18 +0300

    Adding functionality to the delete function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,15 +12,27 @@ export class AppComponent {
 	todos = ["Angular4","PouchDb","CouchDb"];
 
 	// new item holder
-	newItem = "hay";
+	newItem = "";
 
 	// function to push new items to the array
 	newItemPush(){
+		// check if the input is empty
 		if (this.newItem != "") {
+			// if everything is okay then push/save to the array
 			this.todos.push(this.newItem);
 			this.newItem = "";
 		}else{
+			// show error if the input is empty
 			this.showError = true;
+		}
+	}
+
+	// function to remove items from athe array
+	removeItem(index){
+		// confirm if you would like to delete the item
+		if(confirm("Are you sure you want to delete this item??")){
+			// use a javascript inbuilt method splice() to remove item from the array
+			this.todos.splice(index, 1);
 		}
 	}
 }
