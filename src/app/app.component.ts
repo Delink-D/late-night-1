1bc809ed0a5744ecaa53ad48c32e6dd88ea94026
Delink-D <delinkdesigns@gmail.com>
2017-06-01 00:52:29 +0300

    Adding items from the input fild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+	showError = false;
+
 	// an array to hold all the todos
 	todos = ["Angular4","PouchDb","CouchDb"];
+
+	// new item holder
+	newItem = "hay";
+
+	// function to push new items to the array
+	newItemPush(){
+		if (this.newItem != "") {
+			this.todos.push(this.newItem);
+			this.newItem = "";
+		}else{
+			this.showError = true;
+		}
+	}
 }
